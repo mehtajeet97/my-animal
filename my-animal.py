b2fb46234e71b6884d41e1eebdd627266d317b12
Jeet Mehta <mehta.jeet97@gmail.com>
2024-03-28 17:50:33 -0400

    Second change to my Animal

--- my-animal.py
+++ my-animal.py
@@ -1,3 +1,5 @@
 # This is Dummy File
 
-# There are no animals in the wild. Only the ones within
+# There are no animals in the wild. Only the ones within.
+
+# Most proverbs have animals in them, coz humans learned by observing them
